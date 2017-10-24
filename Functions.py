5536975b8b49abb24cb8d696578d593e25bc42eb
Fernando Silva <fsilva.pt@gmail.com>
2017-10-24 19:49:55 +0100

    do_twice() executes twice the function passed as argument

--- Functions.py
+++ Functions.py
@@ -8,10 +8,30 @@ def right_justify(s):
     :return: None
 
     :Example:
-
     >>> right_justify('monty')
                                                                      monty
     """
     print((70 - len(s)) * " " + s)
 
+def do_twice(f):
+    """
+    Takes a function as an argument and calls it twice.
+
+    :param func f: Function to be called twice
+    :return: None
+
+    :Example:
+    >>> def print_spam():
+    >>>     print('spam')
+    >>> do_twice(print_spam)
+    spam
+    spam
+    """
+    f()
+    f()
+
+def print_spam():
+    print("spam")
+
 right_justify("monty")
+do_twice(print_spam)
