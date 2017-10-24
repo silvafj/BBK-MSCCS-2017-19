a01b2090385ead2e72757ec31df122051b093ee5
Fernando Silva <fsilva.pt@gmail.com>
2017-10-24 19:58:53 +0100

    Added do_four() that calls a function 4 times with the respective extra argument

--- Functions.py
+++ Functions.py
@@ -33,5 +33,24 @@ def print_twice(s):
 	print(s)
 	print(s)
 
+def do_four(f, v):
+    """
+    Takes a function and values as arguments and calls it four times.
+
+    :param func f: Function to be called
+    :param object v: Value to be passed as argument to f
+    :return: None
+
+    :Example:
+    >>> do_four(print, "more spam")
+    more spam
+    more spam
+    more spam
+    more spam
+    """
+    for _ in range(4):
+        f(v)
+
 right_justify("monty")
 do_twice(print_twice, "spam")
+do_four(print, "more spam")
