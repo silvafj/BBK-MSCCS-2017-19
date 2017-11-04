b07c2bb7d57463e0511b46813de0441701b4817e
Fernando Silva <fsilva.pt@gmail.com>
2017-11-04 12:56:27 +0000

    Understand requirements and organize them

--- Hundred.py
+++ Hundred.py
@@ -1,79 +1,132 @@
 """
 Author: Fernando Silva <fdealm02>
-Hundred is ...
+
+Hundred is a turn-based game and the objective is to reach a score of 100.
 """
-
-def main():
-    """ This is where your program will start execution. """
-    pass
-
-
-def instructions():
-    """ Tell the user the rules of the game. """
-    pass
 
 
 def human_move(computer_score, human_score):
     """
-    Tells the user both her current score and the computer's score, and how far
-    behind (or ahead) she is. Then repeatedly asks whether the user wants to
-    roll again. This continues until either:
+    ???
 
-        1. The user decides not to roll again. The function should return the
-        total of the rolls made during this move.
-
-        2. The user rolls a 1. The function should return 0.``
+    :param int computer_score: Computer score
+    :param int human_score: Human score
+    :return: ???
+    :rtype: int
     """
+
+    # Tells the user both her current score and the computer's score, and how far
+    # behind (or ahead) she is. Then repeatedly asks whether the user wants to
+    # roll again. This continues until either:
+    # 1. The user decides not to roll again. The function should return the
+    # total of the rolls made during this move.
+    # 2. The user rolls a 1. The function should return 0.``
     pass
 
 
 def computer_move(computer_score, human_score):
     """
-    The computer rolls some number of times, displays the result of each roll,
-    and the function returns the result (either 0 or the total of the rolls).
+    ???
 
-    The function may use its parameters in order to play more intelligently
-    (for example, it may wish to gamble more agressively if it is behind).
+    :param int computer_score: Computer score
+    :param int human_score: Human score
+    :return: ???
+    :rtype: int
     """
+    # The computer rolls some number of times, displays the result of each roll,
+    # and the function returns the result (either 0 or the total of the rolls).
+    # The function may use its parameters in order to play more intelligently
+    # (for example, it may wish to gamble more agressively if it is behind).
     pass
 
 
 def is_game_over(computer_score, human_score):
     """
-    Returns True if either player has 100 or more, and the players are not tied,
-    otherwise it returns False. (Call this  only after the human's move.)
+    Checks if the game is over.
+
+    :param int computer_score: Computer score
+    :param int human_score: Human score
+    :return: True if either player has 100 or more, and the players are not tied
+    :rtype: bool
     """
+
+    # TODO: (Call this  only after the human's move.)
     pass
 
 
 def roll():
     """
-    Returns a random number in the range 1 to 6, inclusive. To do this, find the
-    random module on https://docs.python.org/3/library/index.html and follow the
-    link to find the randint method.
+    Simulates a die roll and returns a random number.
+
+    :return: Random number between 1 and 6 (inclusive)
+    :rtype: int
     """
+
+    # To do this, find the
+    # random module on https://docs.python.org/3/library/index.html and follow the
+    # link to find the randint method.
     pass
 
 
 def ask_yes_or_no(prompt):
     """
-    Prints the prompt as a question to the user, for example, "Roll again? ".
-    If the user responds with a string whose first character is 'y' or 'Y', the
-    function returns True. If the user responds with a string whose first
-    character is 'n' or 'N', the function returns False.
+    Specialized input to ask confirmation for a question.
 
-    Any other response will cause the question to be repeated until the user
-    provides an acceptable response.
+    :param str prompt: Question to be confirmed
+    :return: The user confirmation
+    :rtype: bool
     """
-    pass
+
+    acceptable = set(["Y", "YES", "N", "NO"])
+
+    choice = ""
+    while choice not in acceptable:
+        print(prompt + " (y/n)?", end=" ")
+        choice = input().upper()
+
+    return choice[0] == "Y"
 
 
 def show_results(computer_score, human_score):
     """
-    Tells whether the human won or lost, and by how much. (Call this when the
-    game has ended.)
+    Prints the game results: which player won and by how much.
+
+    :param int computer_score: Computer score
+    :param int human_score: Human score
+    :return: None
     """
     pass
+
+
+def instructions():
+    """ Prints the welcome screen and game instructions. """
+
+    # ASCII art generated from
+    # http://patorjk.com/software/taag/#p=display&f=Big&t=Lunar%20Lander
+
+    print("""
+            _    _                       _                     _
+           | |  | |                     | |                   | |
+           | |__| |  _   _   _ __     __| |  _ __    ___    __| |
+           |  __  | | | | | | '_ \   / _` | | '__|  / _ \  / _` |
+           | |  | | | |_| | | | | | | (_| | | |    |  __/ | (_| |
+           |_|  |_|  \__,_| |_| |_|  \__,_| |_|     \___|  \__,_|
+
+                        by Fernando Silva <fdealm02>
+
+      TODO: instructions ...
+    """)
+    pass
+
+
+def main():
+    """ Program execution entry point. """
+    instructions()
+
+    # TODO: game loop
+    # ask_yes_or_no("Roll again")
+
+    show_results()
 
 
 # This is required so we can import this module from other scripts without
--- test/test_Hundred.py
+++ test/test_Hundred.py
@@ -1,4 +1,13 @@
+import unittest
+from hundred import *
 
+class TestHundred(unittest.TestCase):
 
-def test_dummy():
-    pass
+    def test_is_game_over(self):
+        pass
+
+    def test_roll(self):
+        pass
+
+    def test_ask_yes_or_no(self):
+        pass
