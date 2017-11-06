e009ebd4b80eea1ee90197e3a0d55b23de4d7862
Fernando Silva <fsilva.pt@gmail.com>
2017-11-06 16:21:23 +0000

    Added first test

--- test/test_Hundred.py
+++ test/test_Hundred.py
@@ -1,10 +1,13 @@
 import unittest
-from hundred import *
+from Hundred import *
 
 class TestHundred(unittest.TestCase):
 
     def test_is_game_over(self):
-        pass
+        self.assertTrue(is_game_over(100, 99))
+        self.assertTrue(is_game_over(99, 100))
+        self.assertFalse(is_game_over(99, 99))
+        self.assertFalse(is_game_over(105, 105))
 
     def test_roll(self):
         pass
