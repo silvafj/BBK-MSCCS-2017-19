9acaec96a483032f91939cc2ca0dc2b400ce3fd1
Fernando Silva <fsilva.pt@gmail.com>
2017-11-21 16:15:37 +0000

    Calculates the root of the digits of `n`.

--- ex07/digitalRoot.py
+++ ex07/digitalRoot.py
@@ -1,2 +1,13 @@
+def digitalSum(n):
+	"""Calculates the sum of the digits of `n`."""
+	if n < 10:
+		return n
+	else:
+		return n % 10 + digitalSum(n // 10)
+
 def digitalRoot(n):
-	pass
+	"""Calculates the root of the digits of `n`."""
+	if n < 10:
+		return n
+	else:
+		return digitalRoot(digitalSum(n))
