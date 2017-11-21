9bc66820b6d84e3179e94e5654bc01101e51754b
Fernando Silva <fsilva.pt@gmail.com>
2017-11-21 14:18:20 +0000

    Exponentiation (based on recursion)

--- ex04/power.py
+++ ex04/power.py
@@ -1,2 +1,11 @@
 def power(a,n):
-	return True
+	"""
+	Return `a` raised to the power `n`.
+	This implementation uses recursion.
+	"""
+	if n == 0:
+		return 1
+	elif n == 1:
+		return a
+	else:
+		return a * power(a, n - 1)
