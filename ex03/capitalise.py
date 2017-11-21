9fe52eb813d53163f32515fe3fa1b5a632ab73b5
Fernando Silva <fsilva.pt@gmail.com>
2017-11-21 14:03:47 +0000

    Uppercase (copied from my snakify)

--- ex03/capitalise.py
+++ ex03/capitalise.py
@@ -1,2 +1,7 @@
 def capitalise(lower_case_word):
-	return True
+	"""Capitalises the first letter of each word."""
+	words = lower_case_word.split()
+	for i in range(len(words)):
+		words[i] = chr(ord(words[i][0]) - 32) + words[i][1:]
+
+	return ' '.join(words)
