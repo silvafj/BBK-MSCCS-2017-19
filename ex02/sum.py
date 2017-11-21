3e8713d90aca95df8eeb51f6cc363f15dc38b2aa
Fernando Silva <fsilva.pt@gmail.com>
2017-11-21 15:22:12 +0000

    Sum of integers between 1 and `n`

--- ex02/sum.py
+++ ex02/sum.py
@@ -1,2 +1,9 @@
 def sum(n):
-	pass
+	"""
+	Returns the sum of integers between 1 and `n` (inclusive).
+	This implementation uses recursion.
+	"""
+	if n == 1:
+		return 1
+	elif n > 1:
+		return n + sum(n - 1)
