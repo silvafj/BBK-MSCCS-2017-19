bf5ae8c60a821ea9f70e33444c1f7b8305c73e84
Fernando Silva <fsilva.pt@gmail.com>
2017-11-22 14:07:21 +0000

    Modified the program according to exercise 1.2

--- exercise1.py
+++ exercise1.py
@@ -1,28 +1,28 @@
 # CODE for EXERCISE 1
 # -------------------
-# 
+#
 # Import the Tkinter package
 #
 from tkinter import *
 
 # This method is called when the button is pressed
 def clicked():
-    print("Clicked")
+    print("Oh No! You've clicked it.")
 
 # Create a main frame with
 #    - a title
 #    - size 200 by 200 pixels
 app = Tk()
-app.title("GUI Example 1")
-app.geometry('200x200')
+app.title("GUI Example 1 - Changed by Fernando")
+app.geometry('800x600')
 
 # Create the button
 #   - with suitable text
 #   - a command to call when the button is pressed
-button1 = Button(app, text="Click Here", command=clicked)
+button1 = Button(app, text="Click Here, really!", command=clicked)
 
-# Make the button visible at the bottom of the frame 
-button1.pack(side='bottom')
+# Make the button visible at the top of the frame
+button1.pack(side='top')
 
 # Start the application running
 app.mainloop()
