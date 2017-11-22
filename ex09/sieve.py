1156c5a86403b9129d2b002095142c9398c84256
Fernando Silva <fsilva.pt@gmail.com>
2017-11-22 12:42:59 +0000

    My first attempt to ex09 (clearly not optimised and too many loops)

--- ex09/sieve.py
+++ ex09/sieve.py
@@ -1,4 +1,18 @@
 from math import sqrt
 
 def sieve(n):
-    pass
+    """The sieve of Eratosthenes finds all prime numbers up to `n`."""
+
+    primes = {i: True for i in range(2, n + 1)}
+
+    i = 2
+    while i <= n:
+        if primes[i]:
+            j = 2
+            while j <= n:
+                if i * j in primes:
+                    primes[i * j] = False
+                j += 1
+        i += 1
+
+    return [k for k, v in primes.items() if v]
