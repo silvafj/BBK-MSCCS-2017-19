20d8ddcc157c31e927a8fc021011271dbc16e996
Fernando Silva <fsilva.pt@gmail.com>
2017-11-22 15:31:44 +0000

    Change the shape, colour and fill

--- exercise4.py
+++ exercise4.py
@@ -20,39 +20,70 @@ app.geometry('400x400')  # OPTIONALLY set the size
 w = Canvas(app, bg='blue')
 w.pack(expand = 1, fill = BOTH)
 
-
-# Add some shapes and text
-# ------------------------
-#   - rectangle(x0, y0, x1, y1, ...):
-#        top left corner at (x0, y0)
-#        bottom right corner at (x1, y1)
-#   - oval(x0, y0, x1, y1, ...), also used for circles
-#   - text(x, y, text="...", ...)
-w.create_oval(140, 110, 200, 170, outline="green", width = 5)
-w.create_text(200, 200, text = "Hello")
-
 # Event bindings
 # --------------
 #   Bind mouse or key press events to function
 #
 
+shape = "rectangle"
+shape_filled = False
+shape_colour = "yellow"
+
 # Handler for a key press event
 #
-def hKey(event):
-    print("Lower case h key pressed")
+def squareKey(event):
+    global shape
+    print("Lower case s key pressed")
+    shape = "rectangle"
 
-# Bind the key press 'h' TO THE WINDOW
+def circleKey(event):
+    global shape
+    print("Lower case c key pressed")
+    shape = "circle"
+
+def filledKey(event):
+    global shape_filled
+    print("Lower case F key pressed")
+    shape_filled = True
+
+def unfilledKey(event):
+    global shape_filled
+    print("Lower case f key pressed")
+    shape_filled = False
+
+def yellowKey(event):
+    global shape_colour
+    print("Lower case y key pressed")
+    shape_colour = "yellow"
+
+def redKey(event):
+    global shape_colour
+    print("Lower case r key pressed")
+    shape_colour = "red"
+
+# Bind the key press TO THE WINDOW
 #   bind has a pattern and a function name
 #   The pattern is "<KeyPress-h>"
 #      ... works for any key
-app.bind("<KeyPress-h>", hKey)
+app.bind("<KeyPress-s>", squareKey)
+app.bind("<KeyPress-c>", circleKey)
+app.bind("<KeyPress-F>", filledKey)
+app.bind("<KeyPress-f>", unfilledKey)
+app.bind("<KeyPress-y>", yellowKey)
+app.bind("<KeyPress-r>", redKey)
 
 # Handler for a mouse click event
 #
 def mouseClick(event):
     print("Mouse click at x =", event.x, "y =", event.y)
-    w.create_rectangle(event.x, event.y, event.x + 100, event.y + 100,
-                       fill="green", outline="red", width = 2)
+    if shape == "rectangle":
+        w.create_rectangle(event.x, event.y, event.x + 100, event.y + 100,
+                           fill=shape_colour if shape_filled else None,
+                           outline=shape_colour, width = 2)
+    elif shape == "circle":
+        w.create_oval(event.x, event.y, event.x + 100, event.y + 100,
+                      fill=shape_colour if shape_filled else None,
+                      outline=shape_colour, width = 2)
 
 # Bind the mouse click TO THE CANVAS
 #   bind has a pattern and a function name
