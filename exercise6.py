e6e50f1b66084feb802e176eb3662dd77c7ca98d
Fernando Silva <fsilva.pt@gmail.com>
2017-11-23 16:22:10 +0000

    Make 3 radiobuttons and update label colour according to the selected radio

--- exercise6.py
+++ exercise6.py
@@ -1,43 +1,29 @@
 # CODE for EXERCISE 6
 # -------------------
-# This exercise introduces  
+# This exercise introduces
 #   * The radio button widget
-#
-
-# MAKE THE FOLLOWING CHANGES
-#   * change the text of the radio buttons to colours
-#   * when the selection is made, change th background colour of the label
 #
 
 from tkinter import *
 
 root = Tk()
 
-# Method handles radio button selection
-#    * Create a string showing which choice selected
-#    * Use this in the label
 def sel():
-   selection = "You selected the option " + str(var.get())
-   label["text"] = selection
+   label["text"] = "You selected the option " + var.get()
+   label["background"] = var.get()
 
-# Integer control variable 
-#   Holds the selection of the radio button 
-var = IntVar()
+var = StringVar()
 
-# Create 2 radio buttons. Each has the same control variable
-#
-R1 = Radiobutton(root, text="Option 1", variable=var, value=1,
-                  command=sel)
-R1.pack( anchor = W )
+for colour in ["Red", "Green", "Blue"]:
+    rb = Radiobutton(root, text=colour, variable=var, value=colour.lower(),
+                     command=sel)
+    rb.pack(anchor = W)
 
-R2 = Radiobutton(root, text="Option 2", variable=var, value=2,
-                  command=sel)
-R2.pack( anchor = W )
-
-
-# Create a label. Intially blank
 label = Label(root)
 label.pack()
+
+var.set("red")
+sel()
 
 # Start the main loop
 root.mainloop()
