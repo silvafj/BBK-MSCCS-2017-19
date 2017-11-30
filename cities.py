ea3591186292f77437f3493904bc5f430872a5e5
Fernando Silva <fsilva.pt@gmail.com>
2017-11-30 15:41:48 +0000

    read_cities()

--- cities.py
+++ cities.py
@@ -1,47 +1,63 @@
+"""
+Author: Fernando Silva <fdealm02>
+
+Implementation of the Travelling Salesman problem: given a list of cities and
+the distances between each pair of cities, calculates the shortest possible
+route that visits each city exactly once and returns to the origin city.
+"""
+
 def read_cities(file_name):
     """
-    Read in the cities from the given `file_name`, and return 
-    them as a list of four-tuples: 
+    Read in the cities from the given `file_name`, and return
+    them as a list of four-tuples:
 
-      [(state, city, latitude, longitude), ...] 
+      [(state, city, latitude, longitude), ...]
 
-    Use this as your initial `road_map`, that is, the cycle 
-
-      Alabama -> Alaska -> Arizona -> ... -> Wyoming -> Alabama.
+    :param str file_name: File containing the cities data
     """
-    pass
-  
+    cities = []
+    with open(file_name) as f:
+        for line in f:
+            state, city, latitude, longitude = line.strip().split("\t")
+            cities.append((state, city, float(latitude), float(longitude)))
+
+    return cities
+
+
 def print_cities(road_map):
     """
-    Prints a list of cities, along with their locations. 
+    Prints a list of cities, along with their locations.
     Print only one or two digits after the decimal point.
     """
     pass
 
+
 def compute_total_distance(road_map):
     """
-    Returns, as a floating point number, the sum of the distances of all 
-    the connections in the `road_map`. Remember that it's a cycle, so that 
+    Returns, as a floating point number, the sum of the distances of all
+    the connections in the `road_map`. Remember that it's a cycle, so that
     (for example) in the initial `road_map`, Wyoming connects to Alabama...
     """
     pass
 
+
 def swap_adjacent_cities(road_map, index):
     """
-    Take the city at location `index` in the `road_map`, and the city at 
-    location `index+1` (or at `0`, if `index` refers to the last element 
-    in the list), swap their positions in the `road_map`, compute the 
-    new total distance, and return the tuple 
+    Take the city at location `index` in the `road_map`, and the city at
+    location `index+1` (or at `0`, if `index` refers to the last element
+    in the list), swap their positions in the `road_map`, compute the
+    new total distance, and return the tuple
 
         (new_road_map, new_total_distance)
     """
     pass
 
+
 def swap_cities(road_map, index1, index2):
     """
-    Take the city at location `index` in the `road_map`, and the 
-    city at location `index2`, swap their positions in the `road_map`, 
-    compute the new total distance, and return the tuple 
+    Take the city at location `index` in the `road_map`, and the
+    city at location `index2`, swap their positions in the `road_map`,
+    compute the new total distance, and return the tuple
 
         (new_road_map, new_total_distance)
 
@@ -50,21 +66,24 @@ def swap_cities(road_map, index1, index2):
     """
     pass
 
+
 def find_best_cycle(road_map):
     """
-    Using a combination of `swap_cities` and `swap_adjacent_cities`, 
-    try `10000` swaps, and each time keep the best cycle found so far. 
+    Using a combination of `swap_cities` and `swap_adjacent_cities`,
+    try `10000` swaps, and each time keep the best cycle found so far.
     After `10000` swaps, return the best cycle found so far.
     """
     pass
 
+
 def print_map(road_map):
     """
-    Prints, in an easily understandable format, the cities and 
-    their connections, along with the cost for each connection 
+    Prints, in an easily understandable format, the cities and
+    their connections, along with the cost for each connection
     and the total cost.
     """
     pass
+
 
 def main():
     """
@@ -72,6 +91,8 @@ def main():
     cycle and prints it out.
     """
     pass
+    print(read_cities('city-data.txt'))
+
 
 if __name__ == "__main__":
     main()
--- cities_test.py
+++ cities_test.py
@@ -1,1 +1,17 @@
+import pytest
+from cities import *
 
+def test_compute_total_distance():
+    pass
+
+
+def test_swap_adjacent_cities():
+    pass
+
+
+def test_swap_cities():
+    pass
+
+
+def test_find_best_cycle():
+    pass
