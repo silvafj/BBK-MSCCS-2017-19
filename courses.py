177ff0b5baacb93da172493de40654b77473fed5
Fernando Silva <fsilva.pt@gmail.com>
2017-12-08 11:55:00 +0000

    Add `BirkbeckCSISCourse`

--- courses.py
+++ courses.py
@@ -1,5 +1,15 @@
 class BirkbeckCourse:
+    """This class represents a course at Birkbeck."""
+
     def __init__(self, department, code, title):
         self.department = department
         self.code = code
         self.title = title
+
+
+class BirkbeckCSISCourse(BirkbeckCourse):
+    """This class represents a CSIS course at Birkbeck."""
+
+    def __init__(self, department, code, title, recorded=False):
+        super().__init__(department, code, title)
+        self.is_recorded = recorded
