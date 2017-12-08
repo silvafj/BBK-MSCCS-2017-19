4ebf53e6579fb5b91ad0dc30bb76841f4fc108f5
Fernando Silva <fsilva.pt@gmail.com>
2017-12-08 12:17:54 +0000

    Add support to track student attendance on `BirkbeckCourse`

--- courses.py
+++ courses.py
@@ -5,6 +5,25 @@ class BirkbeckCourse:
         self.department = department
         self.code = code
         self.title = title
+        self.students = set()
+
+    def mark_attendance(self, *students):
+        """
+        Mark students as present.
+
+        :param *students: Sequence of student names attending the course
+        """
+        self.students.update(set(students))
+
+    def is_present(self, student):
+        """
+        Check if student is present in the course.
+
+        :param str student: Student name
+        :return: Return True, if student was found in the course
+        :rtype: bool
+        """
+        return student in self.students
 
 
 class BirkbeckCSISCourse(BirkbeckCourse):
