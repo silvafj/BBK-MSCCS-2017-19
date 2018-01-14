5a0142caaf62bf7d5700ef7c56bf4217f0a44058
Fernando Silva <fsilva.pt@gmail.com>
2018-01-14 11:24:06 +0000

    Completed exercise four

--- exercise-four/src/main/java/BookClubPoints.java
+++ exercise-four/src/main/java/BookClubPoints.java
@@ -5,27 +5,32 @@ public class BookClubPoints {
      * Method should return the number of points earned based on the number of
      * books purchased
      *
-     * @param numberOfBooksPurchased
-     * @return points earied
+     * @param numberOfBooksPurchased Number of books purchased
+     * @return Points earned
      */
     public static int getPointsEarned(int numberOfBooksPurchased) {
-        // TODO
-        return -100;
+        switch (numberOfBooksPurchased) {
+            case 0:
+                return 0;
+            case 1:
+                return 5;
+            case 2:
+                return 15;
+            case 3:
+                return 30;
+            default:
+                return 60;
+        }
     }
 
     public static void main(String[] args) {
-        // Define variables
-        int numberOfBooksPurchased;
-        int pointsAwarded;
-
         // Create a Scanner object for keyboard input.
         Scanner keyboard = new Scanner(System.in);
 
         // Get the number of books purchased this month.
         System.out.print("How many books have you purchased? ");
-        numberOfBooksPurchased = keyboard.nextInt();
+        int numberOfBooksPurchased = keyboard.nextInt();
 
-        // TODO
-
+        System.out.println("You've been awarded with " + BookClubPoints.getPointsEarned(numberOfBooksPurchased) + " points.");
     }
 }
