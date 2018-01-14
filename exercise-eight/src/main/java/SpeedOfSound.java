1b39c9ff225eb6b6b6d11f9753404120ec1e09ef
Fernando Silva <fsilva.pt@gmail.com>
2018-01-14 16:32:04 +0000

    Completed exercise eight

--- exercise-eight/src/main/java/SpeedOfSound.java
+++ exercise-eight/src/main/java/SpeedOfSound.java
@@ -2,14 +2,28 @@ import java.util.Scanner;
 
 public class SpeedOfSound {
 
+    private static final int AIR_FEET_PER_SEC = 1100;
+    private static final int WATER_FEET_PER_SEC = 4900;
+    private static final int STEEL_FEET_PER_SEC = 16400;
+
     public static void main(String[] args) {
-        double distance = 0.0; // Distance
-        String medium; // To hold "air", "water", or "steel"
+        Scanner scanner = new Scanner(System.in);
 
-        // Create a Scanner object for keyboard input.
-        Scanner keyboard = new Scanner(System.in);
+        System.out.print("Enter one of the following (air, water, or steel): ");
+        String medium = scanner.next().toLowerCase();
+        if (!medium.equals("air") && !medium.equals("water") && !medium.equals("steel")) {
+            System.out.println("Invalid choice!");
+            return;
+        }
 
-        // TODO
+        System.out.print("Enter the distance the sound wave will travel: ");
+        double distance = scanner.nextDouble();
+        if (distance <= 0) {
+            System.out.println("Distance can't be equal or less than 0!");
+            return;
+        }
+
+        System.out.printf("It will take %.4f seconds.", getDistanceTraveledInSeconds(medium, distance));
     }
 
     /**
@@ -21,15 +35,15 @@ public class SpeedOfSound {
      * @throws NullPointerException when medium is null
      */
     static double getDistanceTraveledInSeconds(String medium, double distance) {
-        // TODO
-        return 0.0;
+        switch (medium.toLowerCase()) {
+            case "air":
+                return distance / AIR_FEET_PER_SEC;
+            case "water":
+                return distance / WATER_FEET_PER_SEC;
+            case "steel":
+                return distance / STEEL_FEET_PER_SEC;
+            default:
+                return 0.0;
+        }
     }
 }
-
-/*
-Sample output:
-
-Enter one of the following: air, water, or steel: air
-Enter the distance the sound wave will travel: 4500
-It will take 4.090909090909091 seconds.
- */
