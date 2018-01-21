b9b3d2b698afab55087ed0d24e0b36539b7afa35
Fernando Silva <fsilva.pt@gmail.com>
2018-01-21 20:42:21 +0000

    Completed exercise 12

--- exercise-twelve/src/RandomNumbers.java
+++ exercise-twelve/src/RandomNumbers.java
@@ -1,5 +1,12 @@
+import java.util.ArrayList;
 import java.util.Random;
 
+/**
+ * This class provides functions to obtain random elements from int[] and ArrayList<Integer>.
+ *
+ * Both int[] and ArrayList<Integer> are builtin implementations, where extending their behavior can only be done
+ * either by inheritance/wrapping or composition (as is this class).
+ */
 public class RandomNumbers {
     private static Random generator = new Random();
 
@@ -7,5 +14,19 @@ public class RandomNumbers {
         return low + generator.nextInt(high - low + 1);
     }
 
-    // TODO - two versions of <code>randomElement</code>
+    public static int randomElement(int[] from) {
+        if (from.length > 0) {
+            return from[nextInt(0, from.length - 1)];
+        }
+
+        return 0;
+    }
+
+    public static int randomElement(ArrayList<Integer> from) {
+        if (from.size() > 0) {
+            return from.get(nextInt(0, from.size() - 1));
+        }
+
+        return 0;
+    }
 }
---
+++ exercise-twelve/test/RandomNumbersTest.java
@@ -0,0 +1,27 @@
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import static org.junit.Assert.*;
+
+public class RandomNumbersTest {
+
+    @Test
+    public void nextInt() {
+        assertEquals(0, RandomNumbers.nextInt(0, 0));
+        assertEquals(1, RandomNumbers.nextInt(1, 1));
+    }
+
+    @Test
+    public void randomFromArray() {
+        assertEquals(0, RandomNumbers.randomElement(new int[] {}));
+        assertEquals(1, RandomNumbers.randomElement(new int[] {1}));
+    }
+
+    @Test
+    public void randomFromArrayList() {
+        assertEquals(0, RandomNumbers.randomElement(new ArrayList<>()));
+        assertEquals(1, RandomNumbers.randomElement(new ArrayList<>(Arrays.asList(1))));
+    }
+}
