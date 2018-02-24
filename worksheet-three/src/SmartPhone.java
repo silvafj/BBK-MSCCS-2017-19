b9cb1211fc90da44314fe01517db6a4b9b6b60d4
Fernando Silva <fsilva.pt@gmail.com>
2018-02-24 11:41:02 +0000

    Save money by routing your international calls through the internet!

--- worksheet-three/src/SmartPhone.java
+++ worksheet-three/src/SmartPhone.java
@@ -4,6 +4,19 @@
 public class SmartPhone extends MobilePhone {
 
     /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void call(String number) {
+        if (number.startsWith("00")) {
+            saveLastNumber(number);
+            System.out.println("Calling " + number + " through the internet to save money.");
+        } else {
+            super.call(number);
+        }
+    }
+
+    /**
      * @param url
      */
     public void browseWeb(String url) {
