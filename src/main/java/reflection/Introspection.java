e2559e088cfe0ae5230e7d0768cfa2791494dd31
Fernando Silva <fsilva@fb.com>
2019-01-21 20:47:12 +0000

    Initial skeleton to handle command line arguments

--- src/main/java/reflection/Introspection.java
+++ src/main/java/reflection/Introspection.java
@@ -1,11 +1,33 @@
 package reflection;
 
+import java.util.ArrayList;
+
 /*
   Write a Java program that reads the name of a class from the command line and emits the interface of
   the class in Java syntax (interface or class, modifiers, constructors, methods, fields; no method bodies).
  */
 public class Introspection {
-  public static void main(String[] args) {
 
-  }
+    public static void main(String[] args) {
+        for (String className : args) {
+            for (String item : inspect(className)) {
+                System.out.println(item);
+            }
+        }
+    }
+
+    public static ArrayList<String> inspect(String name) {
+        ArrayList<String> items = new ArrayList<>();
+
+        try {
+            Class type = java.lang.Class.forName(name);
+            items.add(type.getName());
+
+        } catch (ClassNotFoundException e) {
+            items.add(e.toString());
+        }
+
+        return items;
+    }
+
 }
---
+++ src/test/java/reflection/IntrospectionTest.java
@@ -0,0 +1,7 @@
+package reflection;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class IntrospectionTest {
+
+}
