98e05bebfabd1246568e9e4bbe540967f5b62b7c
Fernando Silva <fsilva@fb.com>
2019-01-26 12:39:20 +0000

    FireSensor raises and alarm 10% of the time it is called and drains 20% battery between each poll

--- src/main/java/alarmsystem/SmokeSensor.java
+++ src/main/java/alarmsystem/SmokeSensor.java
@@ -1,23 +1,37 @@
 package alarmsystem;
 
+import java.util.Random;
+
 public class SmokeSensor implements Sensor {
+    private Random random = new Random();
+    private double batteryPercentage = 100.0;
+
     @Override
     public boolean isTriggered() {
-        return false;
+        drainBattery();
+
+        // 10% of the time it is called, it raises an alarm
+        return (random.nextFloat() <= 0.10f);
     }
 
     @Override
     public String getLocation() {
-        return null;
+        return "In the auditorium";
     }
 
     @Override
     public String getSensorType() {
-        return null;
+        return "Smoke sensor";
+    }
+
+    private void drainBattery() {
+        if (batteryPercentage > 0) {
+            batteryPercentage -= (batteryPercentage * 0.2);
+        }
     }
 
     @Override
     public double getBatteryPercentage() {
-        return -1;
+        return batteryPercentage;
     }
 }
