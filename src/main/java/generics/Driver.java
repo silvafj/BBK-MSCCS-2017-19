93a7e603c2364858038581301026cf8937b862b5
Fernando Silva <fsilva@fb.com>
2019-01-26 11:14:37 +0000

    Use dynamic casting

--- src/main/java/generics/Driver.java
+++ src/main/java/generics/Driver.java
@@ -6,17 +6,13 @@ public class Driver {
         // It reduces the amount of code to be written and maintained
         Storage<BankAccount> aStorage = new Storage<>();
         Storage<String> sStorage = new Storage<>();
-    }
 
-    public void depositAmount() {
         Class baCls = BankAccount.class;
         try {
-            // This doesn't compile because we are using reflection to create a new instance of BankAccount
-            // but it is recognised only as Object.
-
-            // It still doesn't compile because now we are missing a casting. newInstance() creates an instance
-            // of BankAccount, but it returns it as Object.
-            BankAccount myAccount = baCls.newInstance();
+            // Dynamic cast is used to enforce that a certain value is from a different type than the original
+            // It will be performed by the JVM (hence the dynamic aspect of it)
+            // This code is not safe and unless properly tested and checked at runtime can cause unattended results
+            BankAccount myAccount = (BankAccount) baCls.newInstance();
             aStorage.setValue(myAccount);
             // Deposit
             myAccount.deposit(15);
