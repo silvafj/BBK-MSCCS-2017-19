84ce94de2cb0b68f586b565744f0f0fda3c4bd7a
Fernando Silva <fsilva@fb.com>
2019-01-26 11:10:25 +0000

    Attempt to set the proper variable type

--- src/main/java/generics/Driver.java
+++ src/main/java/generics/Driver.java
@@ -12,8 +12,11 @@ public class Driver {
         Class baCls = BankAccount.class;
         try {
             // This doesn't compile because we are using reflection to create a new instance of BankAccount
-            // but it is recognised only as Object
-            Object myAccount = baCls.newInstance();
+            // but it is recognised only as Object.
+
+            // It still doesn't compile because now we are missing a casting. newInstance() creates an instance
+            // of BankAccount, but it returns it as Object.
+            BankAccount myAccount = baCls.newInstance();
             aStorage.setValue(myAccount);
             // Deposit
             myAccount.deposit(15);
