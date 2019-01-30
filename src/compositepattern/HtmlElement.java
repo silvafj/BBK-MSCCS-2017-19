740b37a88e5e750436ec84fca96d4b8e2f1b2124
Keith Mannock <keith@dcs.bbk.ac.uk>
2019-01-30 20:15:34 +0000

    reorganisation to remove main/java etc.

---
+++ src/compositepattern/HtmlElement.java
@@ -0,0 +1,27 @@
+package compositepattern;
+
+public class HtmlElement extends HtmlTag {
+  public HtmlElement(String s) {
+    super();
+  }
+
+  @Override
+  public String getTagName() {
+    return null;
+  }
+
+  @Override
+  public void setStartTag(String tag) {
+
+  }
+
+  @Override
+  public void setEndTag(String tag) {
+
+  }
+
+  @Override
+  public void generateHtml() {
+
+  }
+}
---
+++ src/compositepattern/HtmlParentElement.java
@@ -0,0 +1,27 @@
+package compositepattern;
+
+public class HtmlParentElement extends HtmlTag {
+  public HtmlParentElement(String s) {
+    super();
+  }
+
+  @Override
+  public String getTagName() {
+    return null;
+  }
+
+  @Override
+  public void setStartTag(String tag) {
+
+  }
+
+  @Override
+  public void setEndTag(String tag) {
+
+  }
+
+  @Override
+  public void generateHtml() {
+
+  }
+}
