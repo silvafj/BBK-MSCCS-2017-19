460bdb08cf02d2bc907aeb35154662a4a11b8958
Fernando Silva <fsilva@fb.com>
2019-02-02 19:16:08 +0000

    Sort an array of strings by length, reversed length, first character and string containing 'e' first

--- src/Outline.java
+++ src/Outline.java
@@ -1,10 +1,36 @@
 import java.util.Arrays;
+import java.util.Comparator;
 
 public class Outline {
-  public static void main(String... args) { // varargs alternative to String[]
-    Integer[] intArray = {1, 7, 3, 4, 8, 2};
-    System.out.println(Arrays.asList(intArray));
 
-    // Arrays.sort(intArray,.......)
-  }
+    private static final String LOREM_IPSUM = "Lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod";
+
+    public static void main(String... args) {
+        String[] strArray = LOREM_IPSUM.split("\\s+");
+        System.out.println("Original: " + Arrays.asList(strArray));
+
+        Arrays.sort(strArray, Comparator.comparingInt(String::length));
+        System.out.println("Sorted by length: " + Arrays.asList(strArray));
+
+        Arrays.sort(strArray, Comparator.comparingInt(String::length).reversed());
+        System.out.println("Sorted by reverse length: " + Arrays.asList(strArray));
+
+        Arrays.sort(strArray, (a, b) -> a.charAt(0) - b.charAt(0));
+        System.out.println("Sorted by first character: " + Arrays.asList(strArray));
+
+        Arrays.sort(strArray, (a, b) -> {
+            int i1 = a.indexOf('e');
+            int i2 = b.indexOf('e');
+            if (i1 == -1 && i2 == -1) {
+                return 0;
+            } else if (i1 >= 0 && i2 == -1) {
+                return -1;
+            } else if (i1 == -1 && i2 >= 0) {
+                return 1;
+            } else {
+                return i1 - i2;
+            }
+        });
+        System.out.println("Sorted by contain 'e' first: " + Arrays.asList(strArray));
+    }
 }
