ee738eaeea8d8520531bb66950e541c2d8119dd1
Fernando Silva <fsilva@fb.com>
2019-02-07 19:47:12 +0000

    use streams to filter the first two meat dishes

--- src/main/java/Outline.java
+++ src/main/java/Outline.java
@@ -1,10 +1,16 @@
 import java.util.Arrays;
+import java.util.stream.Collectors;
 
 public class Outline {
-  public static void main(String... args) { // varargs alternative to String[]
-    Integer[] intArray = {1, 7, 3, 4, 8, 2};
-    System.out.println(Arrays.asList(intArray));
 
-    // Arrays.sort(intArray,.......)
+  public static void main(String... args) { // varargs alternative to String[]
+    System.out.println("How would you use streams to filter the first two meat dishes?");
+    System.out.println(
+            Dish.menu.stream()
+                    .filter(dish -> dish.getType() == Dish.Type.MEAT)
+                    .filter(dish -> dish.getCalories() >= 500)
+                    .collect(Collectors.toUnmodifiableList())
+    );
   }
+
 }
