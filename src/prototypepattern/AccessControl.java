7530b439a6e3a75d98e0a082deb25b1a84e94b90
Fernando Silva <fsilva@fb.com>
2019-02-21 20:13:50 +0000

    Completed the prototype pattern

--- src/prototypepattern/AccessControl.java
+++ src/prototypepattern/AccessControl.java
@@ -1,11 +1,31 @@
 package prototypepattern;
 
 public class AccessControl implements Prototype {
+  private String controlLevel;
+  private String access;
+
+  public AccessControl(String controlLevel) {
+    this.controlLevel = controlLevel;
+  }
+
   @Override
   public AccessControl clone() throws CloneNotSupportedException {
-    return null;
+    AccessControl cloned = new AccessControl(controlLevel);
+    cloned.setAccess(access);
+    return cloned;
   }
 
   public void setAccess(String access) {
+    this.access = access;
+
+  }
+
+  public String getAccess() {
+    return access;
+
+  }
+
+  public String getControlLevel() {
+    return controlLevel;
   }
 }
--- src/prototypepattern/AccessControlProvider.java
+++ src/prototypepattern/AccessControlProvider.java
@@ -1,7 +1,29 @@
 package prototypepattern;
 
+import java.util.HashMap;
+
 public class AccessControlProvider {
+
+  private static HashMap<String, AccessControl> accessControlMap = new HashMap();
+
+  static {
+    System.out.println("Fetching data from external resources and creating access control objects...");
+    accessControlMap.put("ADMIN", new AccessControl("ADMIN"));
+
+    AccessControl managerUserAccess = new AccessControl("MANAGER");
+    managerUserAccess.setAccess("GENERATE/READ REPORTS");
+    accessControlMap.put("MANAGER", managerUserAccess);
+
+    AccessControl userAccessControl = new AccessControl("USER");
+    userAccessControl.setAccess("DO_WORK");
+    accessControlMap.put("USER", userAccessControl);
+  }
+
   public static AccessControl getAccessControlObject(String controlLevel) {
-    return null;
+    try {
+      return accessControlMap.get(controlLevel).clone();
+    } catch (CloneNotSupportedException e) {
+      return null;
+    }
   }
 }
