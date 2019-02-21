d04fc4be16123cbc3fa4af537ded1ac1289f00a9
Fernando Silva <fsilva@fb.com>
2019-02-21 19:42:08 +0000

    Completed the builder pattern

--- src/builderpattern/CarDirector.java
+++ src/builderpattern/CarDirector.java
@@ -8,7 +8,15 @@ public class CarDirector {
   }
 
   public Car build() {
-    // TODO
+    this.carBuilder.buildBodyStyle();
+    this.carBuilder.buildBreaks();
+    this.carBuilder.buildBodyStyle();
+    this.carBuilder.buildEngine();
+    this.carBuilder.buildFuelType();
+    this.carBuilder.buildPower();
+    this.carBuilder.buildSeats();
+    this.carBuilder.buildWindows();
+
     return this.carBuilder.getCar();
   }
 }
