5428085d834a18c7b187a3d0cbe97303d94cc1cb
Fernando Silva <fsilva@fb.com>
2019-02-23 12:02:37 +0000

    Completed Flyweight design pattern

--- src/flyweight/CPlatform.java
+++ src/flyweight/CPlatform.java
@@ -1,8 +1,13 @@
 package flyweight;
 
 public class CPlatform implements Platform {
+  public CPlatform() {
+    System.out.println("CPlatform object created");
+  }
+
   @Override
   public void execute(Code code) {
-
+    System.out.println("Compiling and executing C code.");
   }
+
 }
