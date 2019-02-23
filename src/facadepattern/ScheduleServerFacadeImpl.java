fb78893084f4459e8c8c9ef19bb10335a83f0b41
Fernando Silva <fsilva@fb.com>
2019-02-23 11:53:39 +0000

    Completed Facade design pattern

--- src/facadepattern/ScheduleServerFacadeImpl.java
+++ src/facadepattern/ScheduleServerFacadeImpl.java
@@ -1,16 +1,29 @@
 package facadepattern;
 
 public class ScheduleServerFacadeImpl implements ScheduleServerFacade {
+  private final ScheduleServer scheduleServer;
+
   public ScheduleServerFacadeImpl(ScheduleServer scheduleServer) {
+    this.scheduleServer = scheduleServer;
   }
 
   @Override
   public void startServer() {
-
+    scheduleServer.startBooting();
+    scheduleServer.readSystemConfigFile();
+    scheduleServer.init();
+    scheduleServer.initializeContext();
+    scheduleServer.initializeListeners();
+    scheduleServer.createSystemObjects();
   }
 
   @Override
   public void stopServer() {
-
+    scheduleServer.releaseProcesses();
+    scheduleServer.destory();
+    scheduleServer.destroySystemObjects();
+    scheduleServer.destoryListeners();
+    scheduleServer.destoryContext();
+    scheduleServer.shutdown();
   }
 }
