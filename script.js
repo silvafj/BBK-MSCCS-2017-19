6c87128b0213d94c6881f96a38437af2acf2e4a4
Fernando Silva <fsilva@fb.com>
2019-02-23 16:44:11 +0000

    Load JSON, parse and generate table with results

--- script.js
+++ script.js
@@ -1,3 +1,42 @@
-window.onload = function() {
-  console.log("window has been loaded")
+function parseResultsFromJSON(tournament) {
+  tournament += ".json";
+  console.log("Requesting JSON from " + tournament);
+
+  $.getJSON(tournament, function (data) {
+    console.log("Generating table for " + data.name + " " + data.year);
+
+    var table = $("#results-table")
+    console.log(table.find("tbody").remove());
+    data.match.forEach(function (match) {
+      var tbody = $("<tbody>");
+
+      match.player.forEach(function (player) {
+        var row = $("<tr>").addClass(player.outcome);
+        row.append($("<td>").html(match.round));
+        row.append($("<td>").addClass("name").html(player.name));
+
+        player.set.forEach(function (set) {
+          row.append($("<td>").text(set));
+        });
+
+        tbody.append(row);
+      });
+
+      table.append(tbody);
+    });
+  });
 }
+
+function formQueryOnSubmit(e) {
+  console.log("Form submit");
+
+  var eleTour = document.getElementById("tournament");
+  parseResultsFromJSON(eleTour.options[eleTour.selectedIndex].value);
+
+  e.preventDefault();
+}
+
+window.addEventListener('load', function () {
+  console.log("Window onload");
+  document.getElementById("form-query").addEventListener("submit", formQueryOnSubmit);
+});
