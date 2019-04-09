99deaa53613012b77d627a4ee31053b98b245bf5
Fernando Silva <fsilva@fb.com>
2019-04-09 16:38:04 +0100

    Completed Template design pattern

--- src/template/ConnectionTemplate.java
+++ src/template/ConnectionTemplate.java
@@ -2,40 +2,40 @@ package template;
 
 public abstract class ConnectionTemplate {
 
-  public final void run() {
-    setDBDriver();
-    setCredentials();
-    connect();
-    prepareStatement();
-    setData();
-    insert();
-    close();
-    destroy();
-  }
+    public final void run() {
+        setDBDriver();
+        setCredentials();
+        connect();
+        prepareStatement();
+        setData();
+        insert();
+        close();
+        destroy();
+    }
 
-  public abstract void setDBDriver();
+    public abstract void setDBDriver();
 
-  public abstract void setCredentials();
+    public abstract void setCredentials();
 
-  public void connect() {
-    System.out.println("Setting connection...");
-  }
+    public void connect() {
+        System.out.println("Setting connection...");
+    }
 
-  public void prepareStatement() {
-    System.out.println("Preparing insert statement...");
-  }
+    public void prepareStatement() {
+        System.out.println("Preparing insert statement...");
+    }
 
-  public abstract void setData();
+    public abstract void setData();
 
-  public void insert() {
-    System.out.println("Inserting data...");
-  }
+    public void insert() {
+        System.out.println("Inserting data...");
+    }
 
-  public void close() {
-    System.out.println("Closing connections...");
-  }
+    public void close() {
+        System.out.println("Closing connections...");
+    }
 
-  public void destroy() {
-    System.out.println("Destroying connection objects...");
-  }
+    public void destroy() {
+        System.out.println("Destroying connection objects...");
+    }
 }
