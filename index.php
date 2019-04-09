0f1631780eee8a09fec7c3e4af42cc5e0a881486
Fernando Silva <fsilva@fb.com>
2019-04-09 11:36:05 +0100

    Add a form to search nominations

--- index.php
+++ index.php
@@ -8,6 +8,51 @@
     <link rel="stylesheet" href="style.css">
   </head>
   <body>
+    <form id="form-query">
+      <fieldset><legend>Search Oscars nominations</legend>
+      <div class="form-item">
+        <label for="year">Year</label>
+        <select name="year-condition">
+          <option value="0">Not restricted</option>
+          <option value="1">Contains</option>
+        </select>
+        <input type="text" name="year">
+      </div>
+      <div class="form-item">
+        <label for="category">Category</label>
+        <select name="category-condition">
+          <option value="0">Not restricted</option>
+          <option value="1">Contains</option>
+        </select>
+        <input type="text" name="category">
+      </div>
+      <div class="form-item">
+        <label for="info">Info</label>
+        <select name="info-condition">
+          <option value="0">Not restricted</option>
+          <option value="1">Contains</option>
+        </select>
+        <input type="text" name="info">
+      </div>
+      <div class="form-item">
+        <label for="nominee">Nominee</label>
+        <select name="nominee-condition">
+          <option value="0">Not restricted</option>
+          <option value="1">Contains</option>
+        </select>
+        <input type="text" name="nominee">
+      </div>
+      <div class="form-item">
+        <label for="won">Won?</label>
+        <select name="won">
+          <option value="all">All</option>
+          <option value="yes">yes</option>
+          <option value="no">No</option>
+        </select>
+      </div>
+      <button type="submit">Query</button>
+      </fieldset>
+    </form>
 <?php
 
 $xml = new DOMDocument;
