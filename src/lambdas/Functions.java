514e8d515086fae8351ba8280a7530435f41f298
Fernando Silva <fsilva@fb.com>
2019-04-10 13:28:30 +0100

    Fixed the missing functions

--- src/lambdas/Functions.java
+++ src/lambdas/Functions.java
@@ -14,7 +14,11 @@ public class Functions {
   public static FunctionMap<Integer, Integer> intFunctionMap() {
     FunctionMap<Integer, Integer> intFunctionMap = new FunctionMap<>();
 
-    // todo: add simple functions to the function map (abs, sng, increment, decrement, square)
+    intFunctionMap.addFunction("abs", Math::abs);
+    intFunctionMap.addFunction("sgn", Integer::signum);
+    intFunctionMap.addFunction("increment", x -> x + 1);
+    intFunctionMap.addFunction("decrement", x -> x - 1);
+    intFunctionMap.addFunction("square", x -> x * x);
 
     return intFunctionMap;
   }
