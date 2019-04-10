e58b908849040fc194f05da7e56d318fffead024
Fernando Silva <fsilva@fb.com>
2019-04-10 14:31:36 +0100

    Reformat the code

--- src/ex01/ex01a/Counter.java
+++ src/ex01/ex01a/Counter.java
@@ -1,20 +1,20 @@
 package ex01.ex01a;
 
 public class Counter implements Runnable {
-  private int counter = 0;
-  private final int rounds = 100000;
+    private final int rounds = 100000;
+    private int counter = 0;
 
-  public void run() {
+    public void run() {
 //    try {
-    for (int i = 0; i < rounds; i++) {
-      counter++;
-    }
+        for (int i = 0; i < rounds; i++) {
+            counter++;
+        }
 //    } catch (InterruptedException e) {
 //      System.out.println ("Interrupted!");
 //    }
-  }
+    }
 
-  public int getCounter() {
-    return counter;
-  }
+    public int getCounter() {
+        return counter;
+    }
 }
--- src/ex01/ex01b/Counter.java
+++ src/ex01/ex01b/Counter.java
@@ -1,28 +1,28 @@
 package ex01.ex01b;
 
 public class Counter implements Runnable {
-  private int counter = 0;
-  private final int rounds = 100000;
+    private final int rounds = 100000;
+    private int counter = 0;
 
-  public void run() {
+    public void run() {
 //    try {
-    int id;
-    String name = Thread.currentThread().getName();
-    id = name.equals("thread1") ? 1 : 2;
-    for (int i = 0; i < rounds; i++) {
-      // Delay here?
-      int tmp = counter;
-      // Perhaps here?
-      counter = tmp + 1;
-      // Or here?
+        int id;
+        String name = Thread.currentThread().getName();
+        id = name.equals("thread1") ? 1 : 2;
+        for (int i = 0; i < rounds; i++) {
+            // Delay here?
+            int tmp = counter;
+            // Perhaps here?
+            counter = tmp + 1;
+            // Or here?
 //      }
 //    } catch (InterruptedException e) {
 //      System.out.println("Interrupted!");
 //    }
+        }
     }
-  }
 
-  public int getCounter() {
-    return counter;
-  }
+    public int getCounter() {
+        return counter;
+    }
 }
